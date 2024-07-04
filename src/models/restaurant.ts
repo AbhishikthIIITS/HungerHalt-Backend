c06415f24c69264541ce61208a52e4e090a32eeb
AbhishikthIIITS <abhiabhishikth18@gmail.com>
2024-07-04 16:16:42 +0530

    updateRestaurant feature added

--- src/models/restaurant.ts
+++ src/models/restaurant.ts
@@ -6,7 +6,6 @@ const menuItemSchema = new mongoose.Schema({
     price: { type: Number, required: true},
 });
 
-
 const restaurantSchema = new mongoose.Schema({
     user:                  { type: mongoose.Schema.Types.ObjectId, ref:"User"},
     restaurantName:        { type: String, required: true},
